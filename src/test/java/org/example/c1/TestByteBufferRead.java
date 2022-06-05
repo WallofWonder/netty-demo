29d6531dc39811c4d8577a12595aa9e8a5440839
ZYF <m13110683916@163.com>
2022-06-05 14:44:49 +0800

    ByteBuffer - 方法演示3

---
+++ src/test/java/org/example/c1/TestByteBufferRead.java
@@ -0,0 +1,32 @@
+package org.example.c1;
+
+import java.nio.ByteBuffer;
+
+import static org.example.c1.ByteBufferUtil.debugAll;
+
+public class TestByteBufferRead {
+    public static void main(String[] args) {
+        ByteBuffer buffer = ByteBuffer.allocate(10);
+        buffer.put(new byte[]{'a', 'b', 'c', 'd'});
+        buffer.flip();
+
+        buffer.get(new byte[4]);
+        debugAll(buffer);
+        buffer.rewind(); // 读指针复位到0，从头开始读
+        buffer.get(new byte[4]);
+        debugAll(buffer);
+        buffer.rewind();
+
+        buffer.get(); // a
+        buffer.get(); // b
+        buffer.mark(); // mark c
+        buffer.get(); // c
+        buffer.get(); // d
+        buffer.reset(); // reset to c
+        System.out.println((char) buffer.get()); // print c
+
+        System.out.println((char) buffer.get(2)); // c
+        System.out.println((char) buffer.get(2)); // c
+        System.out.println((char) buffer.get(2)); // c
+    }
+}
