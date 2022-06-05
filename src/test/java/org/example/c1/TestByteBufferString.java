2031c9bf7c76af2a234d8a8ec5339872ed06c7be
ZYF <m13110683916@163.com>
2022-06-05 14:57:39 +0800

    ByteBuffer - 方法演示4

---
+++ src/test/java/org/example/c1/TestByteBufferString.java
@@ -0,0 +1,31 @@
+package org.example.c1;
+
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
+
+import static org.example.c1.ByteBufferUtil.debugAll;
+
+public class TestByteBufferString {
+    public static void main(String[] args) {
+        // ============ 1. String -> ByteBuffer ===================
+        // 直接使用put
+        ByteBuffer buffer1 = ByteBuffer.allocate(10);
+        buffer1.put("hello".getBytes());
+        debugAll(buffer1);
+        // Charset 会自动切换到读模式
+        ByteBuffer buffer2 = StandardCharsets.UTF_8.encode("hello");
+        debugAll(buffer2);
+        // wrap 会自动切换到读模式
+        ByteBuffer buffer3 = ByteBuffer.wrap("hello".getBytes());
+        debugAll(buffer3);
+
+        // ============ 2. ByteBuffer -> String ===================
+        // tip: 第一种直接使用put的方法得到的buffer要先切换到读模式
+        buffer1.flip();
+        String str1 = StandardCharsets.UTF_8.decode(buffer1).toString();
+        System.out.println(str1);
+
+        String str2 = StandardCharsets.UTF_8.decode(buffer2).toString();
+        System.out.println(str2);
+    }
+}
