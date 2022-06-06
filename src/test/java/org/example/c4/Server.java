6ef8bacaf652cba63a61405a4939117a23c963cb
ZYF <m13110683916@163.com>
2022-06-06 12:16:50 +0800

    noi.selector - 正确处理Client断开

--- src/test/java/org/example/c4/Server.java
+++ src/test/java/org/example/c4/Server.java
@@ -11,6 +11,7 @@ import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
 import java.util.Iterator;
 import java.util.Scanner;
+import java.util.Set;
 
 import static org.example.c2.ByteBufferUtil.debugRead;
 
@@ -26,7 +27,7 @@ public class Server {
         // 事件发生后可以通过 SelectionKey 得到事件发生的信息
         // 这里 key 只关注 accept 事件
         SelectionKey sscKey = ssc.register(selector, SelectionKey.OP_ACCEPT, null);
-        log.debug("register key: {}", sscKey);
+        log.debug("Register key: {}", sscKey);
 
         ssc.bind(new InetSocketAddress(8888));
         while (true) {
@@ -47,11 +48,27 @@ public class Server {
                     sc.register(selector, SelectionKey.OP_READ, null);
                     log.debug("Connection established: {}", sc);
                 } else if (key.isReadable()) {
-                    SocketChannel sc = (SocketChannel) key.channel();
-                    ByteBuffer buffer = ByteBuffer.allocate(128);
-                    sc.read(buffer);
-                    buffer.flip();
-                    debugRead(buffer);
+                    // 客户端断开后，也会触发一次 read 事件，key 就是48行获得的 key
+                    try {
+                        SocketChannel sc = (SocketChannel) key.channel();
+                        ByteBuffer buffer = ByteBuffer.allocate(128);
+                        int read = sc.read(buffer);
+                        if (read == -1) {
+                            // 正常断开的情况，read 为 -1
+                            // 要把 key 从 selector 中注销，下次select()的时候会被删除
+                            log.debug("Client closed");
+                            key.cancel();
+                            sc.close();
+                        } else {
+                            // 普通的 read 事件
+                            buffer.flip();
+                            debugRead(buffer);
+                        }
+                    } catch (IOException e) {
+                        // 异常断开的情况，抛出异常
+                        log.error(e.getMessage());
+                        key.cancel();
+                    }
                 }
                 // 因为 selectedKeys 不会主动将处理过事件的 key 移除
                 // 所以处理完毕，必须将负责监听此事件的 key 移除
