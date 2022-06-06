331e5308b2d9e818d15cbcdd5298afa90721283a
ZYF <m13110683916@163.com>
2022-06-06 09:09:49 +0800

    NOI - 非阻塞模式调试

--- src/test/java/org/example/c4/Server.java
+++ src/test/java/org/example/c4/Server.java
@@ -20,24 +20,28 @@ public class Server {
         ByteBuffer buffer = ByteBuffer.allocate(16);
         // 1. 创建服务器
         ServerSocketChannel ssc  = ServerSocketChannel.open();
+        ssc.configureBlocking(false); // 关闭ServerSocketChannel的阻塞模式
         // 2. 绑定监听端口
         ssc.bind(new InetSocketAddress(8888));
         // 3.1 建立连接集合
         List<SocketChannel> channels = new ArrayList<>();
         while (true) {
             // 3.2 建立连接, SocketChannel用于通信s
-            log.debug("Connecting...");
-            SocketChannel sc = ssc.accept(); // 阻塞方法
-            log.debug("Connected...{}" ,sc);
-            channels.add(sc);
-            // 4. 接收客户端的数据
+            SocketChannel sc = ssc.accept(); // 非阻塞，线程继续运行，若没有连接建立，sc为null
+            if (sc != null) {
+                log.debug("Connected...{}" ,sc);
+                sc.configureBlocking(false); // 关闭SocketChannel的阻塞模式
+                channels.add(sc);
+            }
             for (SocketChannel channel : channels) {
-                log.debug("Before reading...{}", channel);
-                channel.read(buffer); // 阻塞方法
-                buffer.flip();
-                debugRead(buffer);
-                buffer.clear();
-                log.debug("After reading...{}", channel);
+                // 4. 接收客户端的数据
+                int read = channel.read(buffer);// 非阻塞，没有读到数据则返回0
+                if (read > 0) {
+                    buffer.flip();
+                    debugRead(buffer);
+                    buffer.clear();
+                    log.debug("After reading...{}", channel);
+                }
             }
         }
     }
