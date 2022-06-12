9d580c2a66f686454d1fe151536253a17f56a21d
ZYF <m13110683916@163.com>
2022-06-12 19:43:32 +0800

    noi - 异步例子

---
+++ src/test/java/org/example/c5/AioFileChannel.java
@@ -0,0 +1,42 @@
+package org.example.c5;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.AsynchronousFileChannel;
+import java.nio.channels.CompletionHandler;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
+import static org.example.c2.ByteBufferUtil.debugAll;
+
+@Slf4j
+public class AioFileChannel {
+    public static void main(String[] args) {
+        try (AsynchronousFileChannel channel = AsynchronousFileChannel.open(Paths.get("data.txt"), StandardOpenOption.READ)) {
+            ByteBuffer buffer = ByteBuffer.allocate(16);
+            log.debug("Read begin...");
+            // channel.read() 会创建一个守护线程读取文件
+            channel.read(buffer, 0, buffer, new CompletionHandler<Integer, ByteBuffer>() {
+                @Override
+                public void completed(Integer result, ByteBuffer attachment) {
+                    log.debug("Read completed...");
+                    attachment.flip();
+                    debugAll(attachment);
+                }
+
+                @Override
+                public void failed(Throwable exc, ByteBuffer attachment) {
+                    exc.printStackTrace();
+                }
+            });
+            log.debug("Read end...");
+            // 如果主线程很快退出了，守护线程会跟着结束，来不及执行debugAll()，从而没有输出
+            // 所以在主线程加一行阻塞代码 System.in.read();
+            System.in.read();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
