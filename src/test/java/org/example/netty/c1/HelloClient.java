299f7f57f766783066b8bcb4caf85bdf16aa1269
ZYF <m13110683916@163.com>
2022-06-12 21:44:48 +0800

    netty入门 - HelloClient

---
+++ src/test/java/org/example/netty/c1/HelloClient.java
@@ -0,0 +1,26 @@
+package org.example.netty.c1;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.string.StringEncoder;
+
+public class HelloClient {
+    public static void main(String[] args) throws InterruptedException {
+        new Bootstrap()
+                .group(new NioEventLoopGroup())
+                .channel(NioSocketChannel.class)
+                .handler(new ChannelInitializer<NioSocketChannel>() {
+                    @Override
+                    protected void initChannel(NioSocketChannel nioSocketChannel) throws Exception {
+                        nioSocketChannel.pipeline()
+                                .addLast(new StringEncoder()); // 将字符串编码为 ByteBuf
+                    }
+                })
+                .connect("localhost", 8888)
+                .sync()
+                .channel()
+                .writeAndFlush("hello, world");
+    }
+}
