863024e7820a85781c426731d4201d032436f5da
ZYF <m13110683916@163.com>
2022-07-02 14:49:52 +0800

    netty入门 - EventLoop-分工细化

--- src/test/java/org/example/netty/c3/EventLoopServer.java
+++ src/test/java/org/example/netty/c3/EventLoopServer.java
@@ -2,9 +2,7 @@ package org.example.netty.c3;
 
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.buffer.ByteBuf;
-import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.ChannelInboundHandlerAdapter;
-import io.netty.channel.ChannelInitializer;
+import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
@@ -15,13 +13,23 @@ import java.nio.charset.StandardCharsets;
 @Slf4j
 public class EventLoopServer {
     public static void main(String[] args) {
+        // 细分2：创建一个专门处理耗时操作的EventGroup
+        EventLoopGroup group = new DefaultEventLoopGroup();
         new ServerBootstrap()
-                .group(new NioEventLoopGroup())
+                // 细分1：boss(负责 Accept)和 worker(负责SocketChannel的读写)
+                .group(new NioEventLoopGroup(), new NioEventLoopGroup(2))
                 .channel(NioServerSocketChannel.class)
                 .childHandler(new ChannelInitializer<NioSocketChannel>() {
                     @Override
                     protected void initChannel(NioSocketChannel nioSocketChannel) {
-                        nioSocketChannel.pipeline().addLast(new ChannelInboundHandlerAdapter() {
+                        nioSocketChannel.pipeline().addLast("handler1", new ChannelInboundHandlerAdapter() {
+                            @Override
+                            public void channelRead(ChannelHandlerContext ctx, Object msg) {
+                                ByteBuf buf = (ByteBuf) msg;
+                                log.debug(buf.toString(StandardCharsets.UTF_8));
+                                ctx.fireChannelRead(msg); // 将消息传递给下一个handler
+                            }
+                        }).addLast(group, "handler2", new ChannelInboundHandlerAdapter() {
                             @Override
                             public void channelRead(ChannelHandlerContext ctx, Object msg) {
                                 ByteBuf buf = (ByteBuf) msg;
