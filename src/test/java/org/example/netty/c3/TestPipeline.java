93ebff6453e3943580325ae8bff3453615ff8097
ZYF <m13110683916@163.com>
2022-07-12 15:30:00 +0800

    netty入门 - netty-inbound-handler

--- src/test/java/org/example/netty/c3/TestPipeline.java
+++ src/test/java/org/example/netty/c3/TestPipeline.java
@@ -1,11 +1,16 @@
 package org.example.netty.c3;
 
 import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
 import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
+
+import java.nio.charset.StandardCharsets;
 
 @Slf4j
 public class TestPipeline {
@@ -26,21 +31,27 @@ public class TestPipeline {
                                     @Override
                                     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
                                         log.debug("1");
-                                        super.channelRead(ctx, msg);
+                                        ByteBuf buf = (ByteBuf) msg;
+                                        String name = buf.toString(StandardCharsets.UTF_8);
+                                        // 将 name 传递到 下一个 InboundHandler（h2）
+                                        super.channelRead(ctx, name);
                                     }
                                 })
                                 .addLast("h2", new ChannelInboundHandlerAdapter() {
                                     @Override
                                     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
                                         log.debug("2");
-                                        super.channelRead(ctx, msg);
+                                        // h2得到的是h1处理后的msg（String name）
+                                        // 初始化一个Student对象
+                                        Student student = new Student(msg.toString());
+                                        // 将 student 传递到 下一个 InboundHandler（h3）
+                                        super.channelRead(ctx, student);
                                     }
                                 })
                                 .addLast("h3", new ChannelInboundHandlerAdapter() {
                                     @Override
                                     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-                                        log.debug("3");
-                                        super.channelRead(ctx, msg);
+                                        log.debug("3, 结果：{}, class:{}", msg, msg.getClass());
                                         // 发送一些数据，触发outbound handler
                                         // 触发顺序是反向pipeline： h4 <- h5 <- h6
                                         ch.writeAndFlush(ctx.alloc().buffer().writeBytes("server...".getBytes()));
@@ -71,5 +82,11 @@ public class TestPipeline {
                                 });
                     }
                 }).bind(8888);
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class Student {
+        private String name;
     }
 }
