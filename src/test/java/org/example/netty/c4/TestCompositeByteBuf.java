b79529dc78eaed8c9076dc011b93cdcc63854aff
ZYF <m13110683916@163.com>
2022-07-14 15:42:35 +0800

    netty入门 - ByteBuf - CompositeByteBuf

---
+++ src/test/java/org/example/netty/c4/TestCompositeByteBuf.java
@@ -0,0 +1,22 @@
+package org.example.netty.c4;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufAllocator;
+import io.netty.buffer.CompositeByteBuf;
+
+import static org.example.netty.c4.TestByteBuf.log;
+
+public class TestCompositeByteBuf {
+    public static void main(String[] args) {
+        ByteBuf buf1 = ByteBufAllocator.DEFAULT.buffer();
+        buf1.writeBytes(new byte[]{1, 2, 3, 4, 5});
+
+        ByteBuf buf2 = ByteBufAllocator.DEFAULT.buffer();
+        buf2.writeBytes(new byte[]{6, 7, 8, 9, 10});
+
+        CompositeByteBuf buf = ByteBufAllocator.DEFAULT.compositeBuffer();
+        buf.addComponents(true, buf1, buf2);
+        log(buf);
+
+    }
+}
