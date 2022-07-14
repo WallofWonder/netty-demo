990d78f3b32e3e0f71cbf4b8233e186a166898c5
ZYF <m13110683916@163.com>
2022-07-14 16:34:21 +0800

    netty入门 - 练习 - EchoServer EchoClient

---
+++ src/test/java/org/example/netty/c4/EchoServer.java
@@ -0,0 +1,39 @@
+package org.example.netty.c4;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+
+import java.nio.charset.Charset;
+
+public class EchoServer {
+    public static void main(String[] args) {
+        new ServerBootstrap()
+                .group(new NioEventLoopGroup())
+                .channel(NioServerSocketChannel.class)
+                .childHandler(new ChannelInitializer<NioSocketChannel>() {
+                    @Override
+                    protected void initChannel(NioSocketChannel ch) throws Exception {
+                        ch.pipeline().addLast(new LoggingHandler(LogLevel.DEBUG))
+                                .addLast(new ChannelInboundHandlerAdapter() {
+                                    @Override
+                                    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+                                        ByteBuf buf = (ByteBuf) msg;
+                                        System.out.println(buf.toString(Charset.defaultCharset()));
+
+                                        ByteBuf response = ctx.alloc().buffer();
+                                        response.writeBytes(buf);
+                                        ch.writeAndFlush(response);
+                                    }
+                                });
+                    }
+                }).bind(8888);
+    }
+}
