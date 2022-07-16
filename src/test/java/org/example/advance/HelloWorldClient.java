c0c8ab9214f0685de300682ba91b4dbe1c5a77c8
ZYF <m13110683916@163.com>
2022-07-16 14:49:31 +0800

    netty进阶 - 粘包半包-演示

---
+++ src/test/java/org/example/advance/HelloWorldClient.java
@@ -0,0 +1,48 @@
+package org.example.advance;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class HelloWorldClient {
+    public static void main(String[] args) {
+        NioEventLoopGroup worker = new NioEventLoopGroup();
+        try {
+            ChannelFuture channelFuture = new Bootstrap()
+                    .channel(NioSocketChannel.class)
+                    .group(worker)
+                    .handler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            log.debug("connected...");
+                            ch.pipeline().addLast(new ChannelInboundHandlerAdapter() {
+                                @Override
+                                public void channelActive(ChannelHandlerContext ctx) throws Exception {
+                                    log.debug("sending...");
+                                    // 总共 160 字节数据，分 10 次发送
+                                    for (int i = 0; i < 10; i++) {
+                                        ByteBuf buf = ctx.alloc().buffer(16);
+                                        buf.writeBytes(new byte[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15});
+                                        ctx.writeAndFlush(buf);
+                                    }
+                                }
+                            });
+                        }
+                    }).connect("localhost", 8888).sync();
+            channelFuture.channel().closeFuture().sync();
+
+        } catch (InterruptedException e) {
+            log.error("client error", e);
+        } finally {
+            worker.shutdownGracefully();
+        }
+    }
+}
