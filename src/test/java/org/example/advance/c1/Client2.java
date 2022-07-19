bfd22b47c5d89db8f0d2f7b0a84818f4c1d7d979
ZYF <m13110683916@163.com>
2022-07-19 10:17:52 +0800

    netty进阶 - 粘包半包-定长解码器

---
+++ src/test/java/org/example/advance/c1/Client2.java
@@ -0,0 +1,70 @@
+package org.example.advance.c1;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Arrays;
+import java.util.Random;
+
+@Slf4j
+public class Client2 {
+    public static void main(String[] args) {
+        send();
+        log.debug("finish");
+    }
+
+    public static byte[] fill10Bytes(char c, int len) {
+        byte[] bytes = new byte[10];
+        Arrays.fill(bytes, (byte) '_');
+        for (int i = 0; i < len; i++) {
+            bytes[i] = (byte) c;
+        }
+        System.out.println(new String(bytes));
+        return bytes;
+    }
+
+    public static void send() {
+        NioEventLoopGroup worker = new NioEventLoopGroup();
+        try {
+            Bootstrap bootstrap = new Bootstrap();
+            bootstrap.channel(NioSocketChannel.class);
+            bootstrap.group(worker);
+            bootstrap.handler(new ChannelInitializer<SocketChannel>() {
+                @Override
+                protected void initChannel(SocketChannel ch) throws Exception {
+                    ch.pipeline().addLast(new LoggingHandler(LogLevel.DEBUG));
+                    ch.pipeline().addLast(new ChannelInboundHandlerAdapter() {
+                        @Override
+                        public void channelActive(ChannelHandlerContext ctx) throws Exception {
+                            ByteBuf buf = ctx.alloc().buffer();
+                            char c = '0';
+                            Random r = new Random();
+                            for (int i = 0; i < 10; i++) {
+                                byte[] bytes = fill10Bytes(c, r.nextInt(10) + 1);
+                                c++;
+                                buf.writeBytes(bytes);
+                            }
+                            ctx.writeAndFlush(buf);
+                        }
+                    });
+                }
+            });
+            ChannelFuture channelFuture = bootstrap.connect("localhost", 8888).sync();
+            channelFuture.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            log.error("client error", e);
+        } finally {
+            worker.shutdownGracefully();
+        }
+    }
+}
