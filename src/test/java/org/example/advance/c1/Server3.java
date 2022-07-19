242baff849ba3f9454ce6a416a0b56a5cba3e8ea
ZYF <m13110683916@163.com>
2022-07-19 10:21:01 +0800

    netty进阶 - 粘包半包-行解码器

---
+++ src/test/java/org/example/advance/c1/Server3.java
@@ -0,0 +1,53 @@
+package org.example.advance.c1;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.AdaptiveRecvByteBufAllocator;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.LineBasedFrameDecoder;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Server3 {
+    public static void main(String[] args) {
+        new Server3().start();
+    }
+
+    void start() {
+        NioEventLoopGroup boss = new NioEventLoopGroup();
+        NioEventLoopGroup worker = new NioEventLoopGroup();
+        try {
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap.channel(NioServerSocketChannel.class);
+            serverBootstrap.childOption(ChannelOption.RCVBUF_ALLOCATOR, new AdaptiveRecvByteBufAllocator(16, 16, 16));
+            serverBootstrap.group(boss, worker);
+
+            serverBootstrap.childHandler(new ChannelInitializer<SocketChannel>() {
+                @Override
+                protected void initChannel(SocketChannel ch) throws Exception {
+                    // 先指明定长解码，再设置日志处理器
+                    ch.pipeline().addLast(new LineBasedFrameDecoder(1024));
+                    ch.pipeline().addLast(new LoggingHandler(LogLevel.DEBUG));
+
+                }
+            });
+            ChannelFuture channelFuture = serverBootstrap.bind(8888);
+            log.debug("{} binding...", channelFuture.channel());
+            channelFuture.sync();
+            log.debug("{} bound...", channelFuture.channel());
+            channelFuture.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            log.error("server error", e);
+        } finally {
+            worker.shutdownGracefully();
+            boss.shutdownGracefully();
+        }
+    }
+}
+
