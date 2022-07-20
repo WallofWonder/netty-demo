89426b40e179c68b28fc71bcb12df15cb231e476
ZYF <m13110683916@163.com>
2022-07-20 10:00:00 +0800

    netty进阶 - 协议设计与解析-redis

---
+++ src/test/java/org/example/advance/c2/TestRedis.java
@@ -0,0 +1,75 @@
+package org.example.advance.c2;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.logging.LoggingHandler;
+import lombok.extern.slf4j.Slf4j;
+
+import java.nio.charset.Charset;
+
+/**
+ * 模拟redis协议
+ */
+@Slf4j
+public class TestRedis {
+    private static final byte[] LINE = {13, 10};
+
+
+    public static void main(String[] args) {
+        NioEventLoopGroup worker = new NioEventLoopGroup();
+        try {
+            Bootstrap bootstrap = new Bootstrap();
+            bootstrap.group(worker);
+            bootstrap.channel(NioSocketChannel.class);
+            bootstrap.handler(new ChannelInitializer<SocketChannel>() {
+                @Override
+                protected void initChannel(SocketChannel ch) throws Exception {
+                    ch.pipeline().addLast(new LoggingHandler());
+                    ch.pipeline().addLast(new ChannelInboundHandlerAdapter() {
+                        @Override
+                        public void channelActive(ChannelHandlerContext ctx) throws Exception {
+                            ByteBuf buf = ctx.alloc().buffer();
+                            // 原redis命令：       set name zhangsan
+                            // redis协议传输格式：  *3 \n $3 \n set \n $4 \n name \n $8 \n zhangsan
+                            buf.writeBytes("*3".getBytes());
+                            buf.writeBytes(LINE);
+                            buf.writeBytes("$3".getBytes());
+                            buf.writeBytes(LINE);
+                            buf.writeBytes("set".getBytes());
+                            buf.writeBytes(LINE);
+                            buf.writeBytes("$4".getBytes());
+                            buf.writeBytes(LINE);
+                            buf.writeBytes("name".getBytes());
+                            buf.writeBytes(LINE);
+                            buf.writeBytes("$8".getBytes());
+                            buf.writeBytes(LINE);
+                            buf.writeBytes("zhangsan".getBytes());
+                            buf.writeBytes(LINE);
+                            ctx.writeAndFlush(buf);
+                        }
+
+                        @Override
+                        public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+                            ByteBuf buf = (ByteBuf) msg;
+                            System.out.println(buf.toString(Charset.defaultCharset()));
+                        }
+                    });
+                }
+            });
+            ChannelFuture channelFuture = bootstrap.connect("localhost", 6379).sync();
+            channelFuture.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            log.error("client error", e);
+        } finally {
+            worker.shutdownGracefully();
+        }
+
+    }
+}
