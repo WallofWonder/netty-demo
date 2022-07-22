46d404c2eee38e27556bb362f5f33d1fc5ba1d73
ZYF <m13110683916@163.com>
2022-07-22 15:53:39 +0800

    netty进阶 - 聊天业务-登录

--- src/main/java/org/example/client/ChatClient.java
+++ src/main/java/org/example/client/ChatClient.java
@@ -2,6 +2,8 @@ package org.example.client;
 
 import io.netty.bootstrap.Bootstrap;
 import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
@@ -9,8 +11,11 @@ import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.handler.logging.LogLevel;
 import io.netty.handler.logging.LoggingHandler;
 import lombok.extern.slf4j.Slf4j;
+import org.example.massage.LoginRequestMessage;
 import org.example.protocol.MessageCodecSharable;
 import org.example.protocol.ProtocolFrameDecoder;
+
+import java.util.Scanner;
 
 @Slf4j
 public class ChatClient {
@@ -29,6 +34,26 @@ public class ChatClient {
                     ch.pipeline().addLast(new ProtocolFrameDecoder());
                     ch.pipeline().addLast(LOGGING_HANDLER);
                     ch.pipeline().addLast(MESSAGE_CODEC);
+                    ch.pipeline().addLast("client handler", new ChannelInboundHandlerAdapter() {
+                        @Override
+                        public void channelActive(ChannelHandlerContext ctx) throws Exception {
+                            // 负责接收控制台输入，向服务器发送消息
+                            new Thread(() -> {
+                                Scanner scanner = new Scanner(System.in);
+                                System.out.println("用户名：");
+                                String userName = scanner.nextLine();
+                                System.out.println("密码：");
+                                String password = scanner.nextLine();
+                                LoginRequestMessage message = new LoginRequestMessage(userName, password);
+                                ctx.writeAndFlush(message);
+                            }, "system in").start();
+                        }
+
+                        @Override
+                        public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+                            log.debug("msg: {}", msg);
+                        }
+                    });
                 }
             });
             ChannelFuture channelFuture = bootstrap.connect("localhost", 8888).sync();
--- src/main/java/org/example/server/ChatServer.java
+++ src/main/java/org/example/server/ChatServer.java
@@ -2,15 +2,20 @@ package org.example.server;
 
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInitializer;
+import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.handler.logging.LogLevel;
 import io.netty.handler.logging.LoggingHandler;
 import lombok.extern.slf4j.Slf4j;
+import org.example.massage.LoginRequestMessage;
+import org.example.massage.LoginResponseMessage;
 import org.example.protocol.MessageCodecSharable;
 import org.example.protocol.ProtocolFrameDecoder;
+import org.example.server.service.UserServiceFactory;
 
 /**
  * 聊天服务器
@@ -34,6 +39,16 @@ public class ChatServer {
                     ch.pipeline().addLast(new ProtocolFrameDecoder());
                     ch.pipeline().addLast(LOGGING_HANDLER);
                     ch.pipeline().addLast(MESSAGE_CODEC);
+                    ch.pipeline().addLast(new SimpleChannelInboundHandler<LoginRequestMessage>() {
+                        @Override
+                        protected void channelRead0(ChannelHandlerContext ctx, LoginRequestMessage msg) throws Exception {
+                            String username = msg.getUsername();
+                            String password = msg.getPassword();
+                            boolean success = UserServiceFactory.getUserService().login(username, password);
+                            LoginResponseMessage message = new LoginResponseMessage(success, success ? "登录成功" : "用户名或密码不正确");
+                            ctx.writeAndFlush(message);
+                        }
+                    });
                 }
             });
             ChannelFuture channelFuture = serverBootstrap.bind(8888).sync();
