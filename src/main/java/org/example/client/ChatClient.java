bcc99345fc772259a32c11cdff9672fdec41bbcf
ZYF <m13110683916@163.com>
2022-07-22 15:15:40 +0800

    netty进阶 - 聊天业务-包结构

--- src/main/java/org/example/client/ChatClient.java
+++ src/main/java/org/example/client/ChatClient.java
@@ -1,34 +1,29 @@
-package org.example.server;
+package org.example.client;
 
-import io.netty.bootstrap.ServerBootstrap;
+import io.netty.bootstrap.Bootstrap;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
-import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.handler.logging.LogLevel;
 import io.netty.handler.logging.LoggingHandler;
 import lombok.extern.slf4j.Slf4j;
 import org.example.protocol.MessageCodecSharable;
 import org.example.protocol.ProtocolFrameDecoder;
 
-/**
- * 聊天服务器
- */
 @Slf4j
-public class ChatServer {
+public class ChatClient {
     public static void main(String[] args) {
-        NioEventLoopGroup boss = new NioEventLoopGroup();
-        NioEventLoopGroup worker = new NioEventLoopGroup();
+        NioEventLoopGroup group = new NioEventLoopGroup();
 
         LoggingHandler LOGGING_HANDLER = new LoggingHandler(LogLevel.DEBUG);
         MessageCodecSharable MESSAGE_CODEC = new MessageCodecSharable();
-
         try {
-            ServerBootstrap serverBootstrap = new ServerBootstrap();
-            serverBootstrap.channel(NioServerSocketChannel.class);
-            serverBootstrap.group(boss, worker);
-            serverBootstrap.childHandler(new ChannelInitializer<SocketChannel>() {
+            Bootstrap bootstrap = new Bootstrap();
+            bootstrap.channel(NioSocketChannel.class);
+            bootstrap.group(group);
+            bootstrap.handler(new ChannelInitializer<SocketChannel>() {
                 @Override
                 protected void initChannel(SocketChannel ch) throws Exception {
                     ch.pipeline().addLast(new ProtocolFrameDecoder());
@@ -36,13 +31,12 @@ public class ChatServer {
                     ch.pipeline().addLast(MESSAGE_CODEC);
                 }
             });
-            ChannelFuture channelFuture = serverBootstrap.bind(8888).sync();
+            ChannelFuture channelFuture = bootstrap.connect("localhost", 8888).sync();
             channelFuture.channel().closeFuture().sync();
         } catch (InterruptedException e) {
-            log.error("server error", e);
+            log.error("client error", e);
         } finally {
-            worker.shutdownGracefully();
-            boss.shutdownGracefully();
+            group.shutdownGracefully();
         }
     }
 }
